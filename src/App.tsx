23cd11507af480117d0d66369b240925fad50293
D <araidon0823@gmail.com>
2025-04-27 04:12:13 +0900

    2025/04/27

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css';
+import React from 'react';
+import './App.css'; // スタイルを別ファイルで管理する場合
 
 function App() {
   return (
-    <div >
-        <p>
-          I'm Don Arai.
-        </p>
+    <div>
+      <header>
+        <h1>Don Arai の </h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>荒井 盾 (Don Arai)</h2>
+          <p>大阪大学 基礎工学研究科 博士前期課程学生</p>
+        </section>
+        <section>
+          <h2>所属</h2>
+          <p>大阪大学大学院基礎工学研究科 システム創成専攻 電子光科学領域 量子コンピューティング研究グループ 藤井研究室 
+            [<a href="https://quantphys.org/wp/qinfp/">Link</a>]
+          </p>
+        </section>
+
+      </main>
     </div>
   );
 }
